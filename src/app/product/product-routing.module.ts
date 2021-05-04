b3719e7010269f369415f2fda495389660ed0af8
lumyrani <lumyrani@gmail.com>
2021-05-04 16:20:10 -0500

    product routing module updated

--- src/app/product/product-routing.module.ts
+++ src/app/product/product-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductComponent } from './product.component';
+import { ProductentryComponent } from './productentry/productentry.component';
+import { ProductlistComponent } from './productlist/productlist.component';
 
 
-const routes: Routes = [{ path: '', component: ProductComponent }];
+const routes: Routes = [{ path: '', component: ProductComponent },{ path: 'productentry', component:  ProductentryComponent },{ path: 'productlist', component:  ProductlistComponent}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
